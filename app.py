ee516ae84be4a7ec073b533a4a43801d8bcd921b
LazarevA <andrey.lazarev@icl-services.com>
2020-08-20 09:02:43 +0300

    new methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_restful import Api
 import resources
 
@@ -7,5 +7,6 @@ app.config.from_object('config')
 api = Api(app)
 
 api.add_resource(resources.Item, '/item/<string:name>')
+api.add_resource(resources.ItemList, '/items')
 
 app.run(host='0.0.0.0', port=5000)
--- resources.py
+++ resources.py
@@ -1,8 +1,23 @@
 from flask_restful import Resource
+from flask import request
 
 items = []
 
 class Item(Resource):
     def get(self, name):
-        items.append({"item": name})
-        return items
+        for item in items:
+            if item["name"] == name:
+                return item, 200
+        return {"item": None}, 404
+
+
+    def post(self, name):
+        data = request.get_json()
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
