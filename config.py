4316aacd6dafb37ec75372837423bde58039229a
LazarevA <andrey.lazarev@icl-services.com>
2020-08-24 16:22:17 +0300

    authentication added

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 DEBUG = True
 FLASK_ENV = "Development"
+secret_key = "my_key"
--- resources.py
+++ resources.py
@@ -1,9 +1,18 @@
 from flask_restful import Resource
 from flask import request
+from security import authenticate, identity
+from flask_jwt import JWT, jwt_required
+
+
+from app import app
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+
+    @jwt_required
     def get(self, name):
         for item in items:
             if item["name"] == name:
